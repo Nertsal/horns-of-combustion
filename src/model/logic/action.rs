c2c33e46b86c5cc6345ca2c2835c01102ae38e0c
Nertsal <sasha.kudasov04@gmail.com>
2023-06-11 13:34:11 +0300

    Cant shoot in barrel state

--- src/model/logic/action.rs
+++ src/model/logic/action.rs
@@ -4,25 +4,27 @@ impl Model {
     pub fn player_action(&mut self, action: PlayerAction) {
         match action {
             PlayerAction::Shoot { target_pos } => {
-                #[allow(dead_code)]
-                #[derive(StructQuery)]
-                struct PlayerRef<'a> {
-                    #[query(storage = ".body.collider")]
-                    position: &'a vec2<Coord>,
-                    #[query(optic = "._Some")]
-                    gun: &'a mut Gun,
-                }
+                if let PlayerState::Human = self.player.state {
+                    #[allow(dead_code)]
+                    #[derive(StructQuery)]
+                    struct PlayerRef<'a> {
+                        #[query(storage = ".body.collider")]
+                        position: &'a vec2<Coord>,
+                        #[query(optic = "._Some")]
+                        gun: &'a mut Gun,
+                    }
 
-                let mut query = query_player_ref!(self.actors);
-                let player = query
-                    .get_mut(self.player.actor)
-                    .expect("Player actor not found");
+                    let mut query = query_player_ref!(self.actors);
+                    let player = query
+                        .get_mut(self.player.actor)
+                        .expect("Player actor not found");
 
-                if player.gun.shot_delay <= Time::ZERO {
-                    let pos = *player.position;
-                    player.gun.shot_delay = player.gun.config.shot_delay;
-                    let config = player.gun.config.shot.clone();
-                    self.shoot(pos, target_pos, Fraction::Player, config);
+                    if player.gun.shot_delay <= Time::ZERO {
+                        let pos = *player.position;
+                        player.gun.shot_delay = player.gun.config.shot_delay;
+                        let config = player.gun.config.shot.clone();
+                        self.shoot(pos, target_pos, Fraction::Player, config);
+                    }
                 }
             }
             PlayerAction::SwitchState => {
