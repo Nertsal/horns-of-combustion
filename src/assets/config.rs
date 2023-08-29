ed57c96b6b3d6345971b833aead2465e23f82971
Nertsal <sasha.kudasov04@gmail.com>
2023-08-29 15:37:42 +0300

    Bullets igniting gas config

--- src/assets/config.rs
+++ src/assets/config.rs
@@ -10,6 +10,8 @@ pub struct Config {
     /// Size of the world torus.
     pub world_size: vec2<Coord>,
     pub explosions_affect_projectiles: bool,
+    /// Whether bullets ignite gasoline when passing by.
+    pub bullets_ignite_on_hover: bool,
     pub death_explosion: Option<ExplosionConfig>,
     pub death_drop_heal_chance: R32,
     pub pickups: PickupConfig,
