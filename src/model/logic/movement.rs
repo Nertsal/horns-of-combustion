3e1bc2731b46391e4c1bc165b031b28ce1d4dc67
nertsal <nestralnertsal@gmail.com>
2024-09-21 16:31:23 +0300

    less code

--- src/model/logic/movement.rs
+++ src/model/logic/movement.rs
@@ -3,19 +3,11 @@ use super::*;
 impl Model {
     /// System that moves all bodies in the world according to their velocity.
     pub(super) fn movement(&mut self, delta_time: Time) {
-        struct MoveRef<'a> {
-            position: &'a mut Position,
-            velocity: &'a vec2<Coord>,
-        }
-
-        for body in query!(
+        for (position, &velocity) in query!(
             [self.actors, self.projectiles, self.pickups],
-            MoveRef {
-                position: &mut body.collider.position,
-                velocity: &body.velocity,
-            }
+            (&mut body.collider.position, &body.velocity,)
         ) {
-            body.position.shift(*body.velocity * delta_time);
+            position.shift(velocity * delta_time);
         }
     }
 }
